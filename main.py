04820093fae1a5f0b4810b1efbd40217e8ef347d
Joel Rhode <joelrhode@gmail.com>
2020-03-02 23:04:46 -0600

    initial commit for assignment 1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import base64
+import peewee
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor 
 
 app = Flask(__name__)
 
@@ -16,7 +17,19 @@ def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        try:
+            donor = Donor.get(Donor.name == request.form['donor'])
+        except peewee.DoesNotExist:
+            return render_template('create.jinja2', error='Existing donor not found')
 
+        Donation(donor = donor, value=request.form['value']).save()
+        return redirect(url_for('all'))
+    else:
+        return render_template('create.jinja2')
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
